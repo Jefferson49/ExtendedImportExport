f28492dccb5822728c7c6f08e336d859611d11fc
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2022-05-24 22:30:39 +0200

    Create ExamplePythonScript.py

---
+++ ExamplePythonScript.py
@@ -0,0 +1,3 @@
+import subprocess
+
+process = subprocess.Popen('"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" "http://MY_URL/webtrees/index.php?route=/webtrees/DownloadGedcomWithURL/tree/MY_TREE/filename/MY_FILE_NAME/privacy/MY_PRIVACY_LEVEL"')

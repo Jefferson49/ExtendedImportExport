7b1143d8c366cf8f356a7c0cf82e4c8552b90553
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2022-05-24 06:47:25 +0200

    Update DownloadGedcomWithURL.php
    Update for webtrees 2.1

--- download_gedcom_with_url/DownloadGedcomWithURL.php
+++ download_gedcom_with_url/DownloadGedcomWithURL.php
@@ -54,162 +54,86 @@ declare(strict_types=1);
 
 namespace DownloadGedcomWithURLNamespace;
 
-use Fisharebest\Localization\Locale;
-use Fisharebest\Localization\Locale\LocaleInterface;
-use Fisharebest\Webtrees\I18N;
+use Fig\Http\Message\RequestMethodInterface;
+use Fisharebest\Webtrees\Encodings\UTF8;
 use Fisharebest\Webtrees\Module\AbstractModule;
 use Fisharebest\Webtrees\Module\ModuleCustomInterface;
 use Fisharebest\Webtrees\Module\ModuleCustomTrait;
-use Fisharebest\Webtrees\Module\LanguageEnglishUnitedStates;
-use Fisharebest\Webtrees\Module\LanguageGerman;
-use Fisharebest\Webtrees\Module\ModuleLanguageInterface;
-use Fisharebest\Webtrees\Services\ModuleService;
-use Fisharebest\Webtrees\Session;
-use Fisharebest\Webtrees\Site;
-use Generator;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use function str_contains;
-
-//For Gedcom export
-use Fisharebest\Webtrees\Auth;
-use Fisharebest\Webtrees\GedcomRecord;
-use Fisharebest\Webtrees\Http\ViewResponseTrait;
 use Fisharebest\Webtrees\Registry;
 use Fisharebest\Webtrees\Services\GedcomExportService;
-use Fisharebest\Webtrees\Tree;
-use Illuminate\Database\Capsule\Manager as DB;
-use League\Flysystem\Filesystem;
-use League\Flysystem\ZipArchive\ZipArchiveAdapter;
+use Fisharebest\Webtrees\Validator;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
-use RuntimeException;
-use function addcslashes;
-use function app;
-use function assert;
-use function fclose;
-use function fopen;
-use function pathinfo;
-use function rewind;
-use function strtolower;
-use function tmpfile;
+use Psr\Http\Server\RequestHandlerInterface;
 
-//TreeService
-use Fisharebest\Webtrees\Services\TreeService;
+use function route;
 
 
 
-class DownloadGedcomWithURL extends AbstractModule implements ModuleCustomInterface, MiddlewareInterface {
+class DownloadGedcomWithURL extends AbstractModule implements ModuleCustomInterface, RequestHandlerInterface {
 
     use ModuleCustomTrait;
-  
+ 
+    protected const ROUTE_URL = '/DownloadGedcomWithURL/tree/{tree}/filename/{filename}/privacy/{privacy}'; 
+
+    private GedcomExportService $gedcom_export_service;
+
+
+   /**
+     * DownloadGedcomWithURL constructor.
+     *
+     * @param ChartService $chart_service
+     */
+    public function __construct()
+    {
+		$response_factory = app(ResponseFactoryInterface::class);
+        $psr17factory = new Psr17Factory();
+
+        $this->gedcom_export_service = new GedcomExportService($response_factory, $psr17factory);
+    }
+
     /**
+     * Initialization.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        Registry::routeFactory()->routeMap()
+            ->get(static::class, static::ROUTE_URL, $this)
+            ->allows(RequestMethodInterface::METHOD_POST);
+    }
+	
+	/**
      * How should this module be identified in the control panel, etc.?
      *
      * @return string
      */
+	 
     public function title(): string
     {
         return 'DownloadGedcomWithURL module';
     }
 
-    /**
-     * Code here is executed before and after we process the request/response.
-     * We can block access by throwing an exception.
-     *
-     * @param ServerRequestInterface  $request
-     * @param RequestHandlerInterface $handler
+     /**
+     * @param ServerRequestInterface $request
      *
      * @return ResponseInterface
      */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+	
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        // Code here is executed before we process the request/response.
-	
-		$params = $request->getQueryParams();
-		$download_file_requested = $params['downloadgedcom'] ?? '';
-		
-		if ($download_file_requested !== '') {
+        $tree         = Validator::attributes($request)->tree();
+        $filename     = Validator::attributes($request)->string('filename');
+        $format       = 'gedcom';
+        $privacy      = Validator::attributes($request)->isInArray(['none', 'gedadmin', 'user', 'visitor'])->string('privacy');
+        $encoding     = UTF8::NAME;
+        $line_endings = 'CRLF';
 
-			$tree_id = $params['treeid'] ?? '';
-			$access_level_requested = $params['accesslevel'] ?? '';
-
-			$tree_service = new TreeService();
-			$tree = $tree_service->find(intval($tree_id));
-			assert($tree instanceof Tree);
-
-			$data_filesystem = Registry::filesystem()->data();
-
-			$params = (array) $request->getParsedBody();
-
-			$convert          = (bool) ($params['convert'] ?? false);
-			$media_path       = $params['media-path'] ?? '';
-
-			$access_levels = [
-				'gedadmin' => Auth::PRIV_NONE,
-				'user'     => Auth::PRIV_USER,
-				'visitor'  => Auth::PRIV_PRIVATE,
-				'none'     => Auth::PRIV_HIDE,
-			];
-
-			$encoding     = $convert ? 'ANSI' : 'UTF-8';
-
-			// How to call the downloaded file
-			$download_filename = $download_file_requested;
-
-			// Add .ged extension
-			$download_filename .= '.ged';
-			
-			
-			// Which access level
-			if ($access_level_requested === 'gedadmin') {
-				$access_level = Auth::PRIV_NONE; 
-			}
-			else if ($access_level_requested === 'user') {
-				$access_level = Auth::PRIV_USER; 
-
-			}
-			else if ($access_level_requested === 'visitor') {
-				$access_level = Auth::PRIV_PRIVATE; 
-
-			}
-			else {
-				//Default is 'none'
-				$access_level = Auth::PRIV_HIDE; 
-			}
-		
-			$resource = fopen('php://temp', 'wb+');
-
-			if ($resource === false) {
-				throw new RuntimeException('Failed to create temporary stream');
-			}
-
-			$gedcom_export_service = new GedcomExportService();
-		
-			$gedcom_export_service->export($tree, $resource, true, $encoding, $access_level, $media_path);
-			rewind($resource);
-
-			$charset = $convert ? 'ISO-8859-1' : 'UTF-8';
-
-			$stream_factory = app(StreamFactoryInterface::class);
-			assert($stream_factory instanceof StreamFactoryInterface);
-
-			$http_stream = $stream_factory->createStreamFromResource($resource);
-
-			/** @var ResponseFactoryInterface $response_factory */
-			$response_factory = app(ResponseFactoryInterface::class);
-
-			return $response_factory->createResponse()
-				->withBody($http_stream)
-				->withHeader('Content-Type', 'text/x-gedcom; charset=' . $charset)
-				->withHeader('Content-Disposition', 'attachment; filename="' . addcslashes($download_filename, '"') . '"');
-		}
-
-					
-        // Generate the response.
-        return $handler->handle($request);	
-	
+        return $this->gedcom_export_service->downloadResponse($tree, true, $encoding, $privacy, $line_endings, $filename, $format);
     }
+
 }

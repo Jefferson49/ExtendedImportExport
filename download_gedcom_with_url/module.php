d8ce7997dfe2b31a44315530779326caac954002
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2022-06-04 18:29:27 +0200

    Add files via upload

--- download_gedcom_with_url/module.php
+++ download_gedcom_with_url/module.php
@@ -20,33 +20,10 @@
  *
  * DownloadGedcomWithURL
  *
+ * Github repository: https://github.com/Jefferson49/DownloadGedcomWithURL
+ *
  * A weebtrees(https://webtrees.net) 2.1 custom module to download GEDCOM files on URL requests 
  * with the tree name, GEDCOM file name and authorization provided as parameters within the URL.
- * 
- * Example URL:
- * http://MY_URL/webtrees/index.php?route=/webtrees/DownloadGedcomWithURL/tree/MY_TREE/filename/MY_FILENAME/privacy/MY_PRIVACY_LEVEL
- *
- * MY_TREE specifies the webtrees tree name
- *
- * M>_FILENAME has to be provided without .ged extension
- * i.e. use my_file instead of my_file.ged
- *
- * For MY_PRIVACY_LEVEL, the following values can be used
- *  	gedadmin
- * 		user 
- * 		visitor  
- *		none     (Default)
- *
- * Note:
- * The Gedcom file will always be downloaded from the last tree, which was used
- * in the frontend.
- * 
- * IMPORTANT SECURITY NOTE:
- * Please note that installing this module will enable everyone who can reach the
- * webtrees URL to download the GEDCOM files from webtrees. This even works if no user
- * is logged in. Therefore, you should only consider to use this module in private 
- * networks etc.
- *
  *
  */
  

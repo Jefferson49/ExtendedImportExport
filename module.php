f3ec320079e03299e6460893c55238b1cbf4109c
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2022-07-23 13:08:40 +0200

    Reorganization of folder structure

---
+++ module.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2022 webtrees development team
+ * Copyright (C) 2022 Webmaster @ Familienforschung Hemprich, 
+ *                    <http://www.familienforschung-hemprich.de>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * DownloadGedcomWithURL
+ *
+ * Github repository: https://github.com/Jefferson49/DownloadGedcomWithURL
+ *
+ * A weebtrees(https://webtrees.net) 2.1 custom module to download GEDCOM files on URL requests 
+ * with the tree name, GEDCOM file name and authorization provided as parameters within the URL.
+ *
+ */
+ 
+
+declare(strict_types=1);
+
+namespace DownloadGedcomWithURLNamespace;
+
+require __DIR__ . '/DownloadGedcomWithURL.php';
+
+return new DownloadGedcomWithURL();

442b54f9fd3485819deba488ba60a80043631143
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2022-12-30 23:31:36 +0100

    Adopted standard interfaces, methods, etc. for custom module

--- DownloadGedcomWithURL.php
+++ DownloadGedcomWithURL.php
@@ -32,11 +32,14 @@ declare(strict_types=1);
 namespace DownloadGedcomWithURLNamespace;
 
 use Fig\Http\Message\RequestMethodInterface;
-use Fisharebest\Webtrees\Encodings\UTF8;
-use Fisharebest\Webtrees\Encodings\UTF16BE;
+use Fig\Http\Message\StatusCodeInterface;
+use Fisharebest\Localization\Translation;
 use Fisharebest\Webtrees\Encodings\ANSEL;
 use Fisharebest\Webtrees\Encodings\ASCII;
+use Fisharebest\Webtrees\Encodings\UTF16BE;
+use Fisharebest\Webtrees\Encodings\UTF8;
 use Fisharebest\Webtrees\Encodings\Windows1252;
+use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Module\AbstractModule;
 use Fisharebest\Webtrees\Module\ModuleCustomInterface;
 use Fisharebest\Webtrees\Module\ModuleCustomTrait;
@@ -44,25 +47,41 @@ use Fisharebest\Webtrees\Registry;
 use Fisharebest\Webtrees\Services\GedcomExportService;
 use Fisharebest\Webtrees\Services\GedcomImportService;
 use Fisharebest\Webtrees\Services\TreeService;
-use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Tree;
 use Fisharebest\Webtrees\View;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
+use RuntimeException;
 
 class DownloadGedcomWithURL extends AbstractModule implements ModuleCustomInterface, RequestHandlerInterface {
 
     use ModuleCustomTrait;
  
-    protected const ROUTE_URL = '/DownloadGedcomWithURL'; 
-
     private GedcomExportService $gedcom_export_service;
 
     private Tree $download_tree;
+
+	//Custom module version
+	public const CUSTOM_VERSION = '3.0.0';
+
+	//Route
+	protected const ROUTE_URL = '/DownloadGedcomWithURL'; 
+
+	//Github repository
+	public const GITHUB_REPO = 'Jefferson49/DownloadGedcomWithURL';
+
+	//Github API URL to get the information about the latest releases
+	public const GITHUB_API_LATEST_VERSION = 'https://api.github.com/repos/'. self::GITHUB_REPO . '/releases/latest';
+	public const GITHUB_API_TAG_NAME_PREFIX = '"tag_name":"v';
+
+	//Author of custom module
+	public const CUSTOM_AUTHOR = 'Markus Hemprich';
+
 
    /**
      * DownloadGedcomWithURL constructor.
@@ -83,7 +102,7 @@ class DownloadGedcomWithURL extends AbstractModule implements ModuleCustomInterf
     public function boot(): void
     {
         Registry::routeFactory()->routeMap()
-            ->get(static::class, static::ROUTE_URL, $this)
+            ->get(static::class, self::ROUTE_URL, $this)
             ->allows(RequestMethodInterface::METHOD_POST);
 
 		// Register a namespace for our views.
@@ -91,23 +110,140 @@ class DownloadGedcomWithURL extends AbstractModule implements ModuleCustomInterf
     }
 	
     /**
-     * How should this module be identified in the control panel, etc.?
+     * {@inheritDoc}
      *
      * @return string
-     */	 
+     *
+     * @see \Fisharebest\Webtrees\Module\AbstractModule::title()
+     */
     public function title(): string
     {
-        return 'DownloadGedcomWithURL custom module';
+        return 'DownloadGedcomWithURL' . I18n::translate('custom module');
     }
 
-	/**
-     * Where does this module store its resources
+    /**
+     * {@inheritDoc}
      *
      * @return string
+     *
+     * @see \Fisharebest\Webtrees\Module\AbstractModule::description()
+     */
+    public function description(): string
+    {
+        /* I18N: Description of the “AncestorsChart” module */
+        return I18N::translate('A custom module to download GEDCOM files on URL requests with the tree name, GEDCOM file name, and authorization provided as parameters within the URL.');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     *
+     * @see \Fisharebest\Webtrees\Module\AbstractModule::resourcesFolder()
      */
     public function resourcesFolder(): string
     {
-		return __DIR__ . '/resources/';
+        return __DIR__ . '/resources/';
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     *
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleAuthorName()
+     */
+    public function customModuleAuthorName(): string
+    {
+        return self::CUSTOM_AUTHOR;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     *
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleVersion()
+     */
+    public function customModuleVersion(): string
+    {
+        return self::CUSTOM_VERSION;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     *
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleLatestVersion()
+     */
+    public function customModuleLatestVersion(): string
+    {
+        // No update URL provided.
+        if (self::GITHUB_API_LATEST_VERSION === '') {
+            return $this->customModuleVersion();
+        }
+        return Registry::cache()->file()->remember(
+            $this->name() . '-latest-version',
+            function (): string {
+                try {
+                    $client = new Client(
+                        [
+                        'timeout' => 3,
+                        ]
+                    );
+
+                    $response = $client->get(self::GITHUB_API_LATEST_VERSION);
+
+                    if ($response->getStatusCode() === StatusCodeInterface::STATUS_OK) {
+                        $content = $response->getBody()->getContents();
+                        preg_match_all('/' . self::GITHUB_API_TAG_NAME_PREFIX . '\d+\.\d+\.\d+/', $content, $matches, PREG_OFFSET_CAPTURE);
+
+                        $version = $matches[0][0][0];
+                        $version = substr($version, strlen(self::GITHUB_API_TAG_NAME_PREFIX));
+
+                        return $version;
+                    }
+                } catch (GuzzleException $ex) {
+                    // Can't connect to the server?
+                }
+
+                return $this->customModuleVersion();
+            },
+            86400
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     *
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleSupportUrl()
+     */
+    public function customModuleSupportUrl(): string
+    {
+        return 'https://github.com/' . self::GITHUB_REPO;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param string $language
+     *
+     * @return array
+     *
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customTranslations()
+     */
+    public function customTranslations(string $language): array
+    {
+        $lang_dir   = $this->resourcesFolder() . 'lang/';
+        $file       = $lang_dir . $language . '.mo';
+        if (file_exists($file)) {
+            return (new Translation($file))->asArray();
+        } else {
+            return [];
+        }
     }
 
      /**

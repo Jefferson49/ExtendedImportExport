e0d19abed081ad42a1b8b1f76db1407ace9654d2
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2023-04-12 20:15:08 +0200

    Deleted earlier commit: Remove "<" and ">" in <user defined> name types if converting to OTHER/PHRASE

--- GedcomSevenExportService.php
+++ GedcomSevenExportService.php
@@ -478,8 +478,7 @@ class GedcomSevenExportService
 				//Use OTHER/PHRASE instead
 				else {
 					$search =  $level  . " " . $level2_tag . " " . $found_type;
-                    $found_type_without_brackets = str_replace(['<', '>'], ['', ''], $found_type);    //for user defined types: <user defined>
-					$replace = $level  . " " . $level2_tag . " OTHER\n" . $level + 1 . " PHRASE " . $found_type_without_brackets;
+					$replace = $level  . " " . $level2_tag . " OTHER\n" . $level + 1 . " PHRASE " . $found_type;
 					$gedcom = str_replace($search, $replace, $gedcom);
 				}
 			}	

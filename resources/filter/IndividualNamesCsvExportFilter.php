cdd16ffcf8429f11ee2a4385c3a81be67706ed34
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-06-02 20:12:00 +0200

    Added filter example for inidivudal names CSV export

---
+++ resources/filter/IndividualNamesCsvExportFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jefferson49\Webtrees\Module\DownloadGedcomWithURL;
+
+/**
+ * Tag definitions and regular expressions for export filter
+ */
+class IndividualNamesCsvExportFilter implements ExportFilterInterface
+{
+    use ExportFilterTrait;
+    public const EXPORT_FILTER = [
+      
+      //GEDCOM tag to be exported => Regular expression to be applied for the chosen GEDCOM tag
+      //                             ["search pattern" => "replace pattern"],
+      'HEAD'                      => [".*\n" => "Surname,Given names\n"],
+
+      'INDI'                      => [".*\n" => ""],
+      'INDI:NAME'                 => ["1 NAME (.*[^ ])? ?\/(.*)\/" => "\"$2\",\"$1\""],
+   ];
+}

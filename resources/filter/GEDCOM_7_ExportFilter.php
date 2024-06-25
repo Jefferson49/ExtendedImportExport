eae6c74416f1219ca45dc8e44080eb8b52cc3c12
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-06-25 06:00:58 +0200

    Refactoring of GEDCOM 7 filter: step 3

--- resources/filter/GEDCOM_7_ExportFilter.php
+++ resources/filter/GEDCOM_7_ExportFilter.php
@@ -45,12 +45,9 @@ class GEDCOM_7_ExportFilter extends AbstractExportFilter implements ExportFilter
 		'*:RIN'                     => ["1 RIN (.[^\n]+)" => "1 EXID $1\n2 TYPE https://gedcom.io/terms/v7/$1",],
 
 		//RELA, ROLE, _ASSO	
-		'INDI:ASSO:RELA'	    	=> ["RegExp_macro" => "RELA_GodparentWitness"],
 		'INDI:ASSO'		            => ["RegExp_macro" => "ASSO_RELA"],
-		'FAM:*:_ASSO:RELA'	        => ["RegExp_macro" => "RELA_GodparentWitness"],
-		'FAM:*:_ASSO'	            => ["RegExp_macro" => "ASSO_RELA"],
-		'INDI:*:_ASSO:RELA'	 	    => ["RegExp_macro" => "RELA_GodparentWitness"],
 		'INDI:*:_ASSO'	 	        => ["RegExp_macro" => "ASSO_RELA"],
+		'FAM:*:_ASSO'	            => ["RegExp_macro" => "ASSO_RELA"],
 
 		'*:SOUR:EVEN:ROLE'          => ["RegExp_macro" => "ROLE_GodparentWitness"],
 		'*:*:SOUR:EVEN:ROLE'        => ["RegExp_macro" => "ROLE_GodparentWitness"],
@@ -83,7 +80,7 @@ class GEDCOM_7_ExportFilter extends AbstractExportFilter implements ExportFilter
 		'FAM:*:_WITN'              	=> ["RegExp_macro" => "_GODP_WITN"],
 		'INDI:*:_WITN'             	=> ["RegExp_macro" => "_GODP_WITN"],
 	
-		'FAM:STAT'                 	=> ["1 _STAT (?i)(NOT|NEVER) MARRIED\n" => "1 NO MARR\n"],
+		'FAM:_STAT'                 => ["1 _STAT (?i)(NOT|NEVER) MARRIED\n" => "1 NO MARR\n"],
 										
 		'FAM:MARR:TYPE'            	=> ["2 TYPE (?i)RELIGIOUS" => "2 TYPE RELI"],
 
@@ -93,9 +90,32 @@ class GEDCOM_7_ExportFilter extends AbstractExportFilter implements ExportFilter
 
 		'TRLR'                      => [],
 
-      	//Apply custom conversion to all other records
-     	 '*'                        => ["PHP_function" => "customConvert"],
+      	//Apply custom conversion for ENUM sets
+		'INDI:NAME:TYPE'            => ["PHP_function" => "customConvert"],
+		'INDI:FAMC:STAT'            => ["PHP_function" => "customConvert"],
+
+		'INDI:ADOP:FAMC:ADOP'       => ["PHP_function" => "customConvert"],
+		'INDI:FAMC:PEDI'            => ["PHP_function" => "customConvert"],
+		'INDI:SEX'					=> ["PHP_function" => "customConvert"],
+
+		'OBJE:FILE:FORM:TYPE'       => ["PHP_function" => "customConvert"],
+		'*:OBJE:FILE:FORM:MEDI'     => ["PHP_function" => "customConvert"],
+		'*:*:OBJE:FILE:FORM:MEDI'   => ["PHP_function" => "customConvert"],
+		'*:*:*:OBJE:FILE:FORM:MEDI' => ["PHP_function" => "customConvert"],
+		'SOUR:REPO:CALN:MEDI'     	=> ["PHP_function" => "customConvert"],
+
+		'*:SOUR:QUAY'				=> ["PHP_function" => "customConvert"],
+		//'*:SOUR:EVEN:ROLE'			=> ["PHP_function" => "customConvert"],   	is handled in ROLE_GodparentWitness
+		'*:*:SOUR:QUAY'				=> ["PHP_function" => "customConvert"],
+		//'*:*:SOUR:EVEN:ROLE'		=> ["PHP_function" => "customConvert"],			is handled in ROLE_GodparentWitness
+
+		'*:RESN'					=> ["PHP_function" => "customConvert"],
+		'*:*:RESN'					=> ["PHP_function" => "customConvert"],
+
+		//Export other records
+		'*'							=> [],
    ];
+
    protected const REGEXP_MACROS = [
 		//Name                      => Regular expression to be applied for the chosen GEDCOM tag
 		//                             ["search pattern" => "replace pattern"],
@@ -106,7 +126,8 @@ class GEDCOM_7_ExportFilter extends AbstractExportFilter implements ExportFilter
 										"@#DHEBREW@( |)"     => 'HEBREW ',
 										"@#DFRENCH R@( |)"   => 'FRENCH_R ',
 										"@#DROMAN@( |)"      => 'ROMAN ',
-										"@#DUNKNOWN@( |)"    => 'UNKNOWN ',],
+										"@#DUNKNOWN@( |)"    => 'UNKNOWN ',
+										"PHP_function" => "customConvert"],
 
 		"AgeConversion"				=> ["([\d]) AGE 0([\d]{1,2})y" => "$1 AGE $2y",
 										"([\d]) AGE ([\d]{1,3})y 0(.)m" => "$1 AGE $2y $3m",
@@ -116,177 +137,188 @@ class GEDCOM_7_ExportFilter extends AbstractExportFilter implements ExportFilter
 										"([\d]) AGE (<|>)([\d])" => "$1 AGE $2 $3",
 										"([\d]) AGE (?i)INFANT" => "$1 AGE CHILD"],
 
-		"ASSO_RELA"					=> ["([\d]) (_?)ASSO (.*)\n([\d]) RELA" => "$1 $2ASSO $3\n$4 ROLE"],
+		"ASSO_RELA"					=> ["([\d]) RELA (?i)GODPARENT" => "$1 RELA GODP",
+										"([\d]) RELA (?i)WITNESS" => "$1 RELA WITN",
+										"([\d]) (_?)ASSO ([^\n]*)\n([\d]) RELA" => "$1 ASSO $3\n$4 ROLE",
+										"PHP_function" => "customConvert"],
 
-		"RELA_GodparentWitness"		=> ["([\d]) RELA (?i)GODPARENT" => "$1 ROLE GODP",
-										"([\d]) RELA (?i)WITNESS" => "$1 ROLE WITN",],
-
-		"ROLE_GodparentWitness"		=> ["3 ROLE \((?i)GODPARENT\)" => "3 ROLE GODP",
-										"3 ROLE (?i)GODPARENT" => "3 ROLE GODP",
-										"3 ROLE (?i)WITNESS" => "3 ROLE WITN",],
+		"ROLE_GodparentWitness"		=> ["([\d]) ROLE \((?i)GODPARENT\)" => "$1 ROLE GODP",
+										"([\d]) ROLE (?i)GODPARENT" => "$1 ROLE GODP",
+										"([\d]) ROLE (?i)WITNESS" => "$1 ROLE WITN",
+										"PHP_function" => "customConvert"],
 
 		"SharedNotes"				=> ["([\d]) NOTE @([^@)]+)@" => "$1 SNOTE @$2@"],
 
-		"_GODP_WITN"			      => ["2 _(_GODP|_WITN) (.*)" => "2 ASSO @VOID@\n3 PHRASE $2\n3 ROLE $1"],
+		"_GODP_WITN"			      => ["2 _(GODP|WITN) (.*)" => "2 ASSO @VOID@\n3 PHRASE $2\n3 ROLE $1"],
 	];
 
+	private const ENUMSETS = [
+		"ADOP" => ["HUSB", "WIFE", "BOTH",],
+		"MEDI" => ["AUDIO", "BOOK","CARD", "ELECTRONIC", "FICHE", "FILM", "MAGAZINE", "MANUSCRIPT", "MAP", "NEWSPAPER", "PHOTO", "TOMBSTONE", "VIDEO", "OTHER",],
+		"PEDI" => ["ADOPTED", "BIRTH", "FOSTER", "SEALING", "OTHER",],
+		"QUAY" => ["1", "2", "3",],
+		"RESN" => ["CONFIDENTIAL", "LOCKED", "PRIVACY",],
+		"ROLE" => ["CHIL", "CLERGY", "FATH", "FRIEND", "GODP", "HUSB", "MOTH", "MULTIPLE", "NGHBR", "OFFICIATOR", "PARENT", "SPOU", "WIFE", "WITN", "OTHER",],
+		"SEX" =>  ["M", "F", "X", "U",],
+	];
+
+	private const NESTED_ENUMSETS = [
+		[ "tags" => ["NAME", "TYPE"], "values" => ["AKA", "BIRTH", "IMMIGRANT", "MAIDEN", "MARRIED", "PROFESSIONAL",]],
+		[ "tags" => ["FAMC", "STAT"], "values" => ["CHALLENGED", "DISPROVEN", "PROVEN",]],
+	];
+		
 	/**
 	 * Constructor
 	 *
 	 */      
 	public function __construct() {
 
-      $iana_language_registry_file_name = __DIR__ . '/../../vendor/iana/iana_languages.txt';
-      $iana_language_registry = file_get_contents($iana_language_registry_file_name);
-   
-      //Create language table
-      preg_match_all("/Type: language\nSubtag: ([^\n]+)\nDescription: ([^\n]+)\n/", $iana_language_registry, $matches, PREG_SET_ORDER);
-   
-      foreach ($matches as $match) {
-         $this->language_to_code_table[strtoupper($match[2])]= $match[1];
-      }   
-   }
-
-   /**
-    * Custom conversion of a Gedcom string
-    *
-    * @param string $pattern       The pattern of the filter rule, e. g. INDI:BIRT:DATE
-    * @param string $gedcom        The Gedcom to convert
-    * @param array  $records_list  A list with all xrefs and the related records: array <string xref => Record record>
-    * 
-    * @return string               The converted Gedcom
-    */
-   public function customConvert(string $pattern, string $gedcom, array $records_list): string {
-
-		//Specific dates with slashes in years, eg. 1741/42
-        $preg_pattern = [
-			"/([\d]) DATE ([\d]{0,2})([ ]*)(|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)([ ]*)([\d]{1,4})\/([\d]{2})\n/",
-        ];
-
-        foreach ($preg_pattern as $pattern) {
-
-            preg_match_all($pattern, $gedcom, $matches, PREG_SET_ORDER);
-
-            foreach ($matches as $match) {
-                $level = (int) $match[1];
-
-                $date_value   = $match[2] . $match[3] . $match[4] . $match[5]  . $match[6];
-                $phrase_value = $date_value . "/" . $match[7];
-                $search       = (string) $level . " DATE " . $phrase_value;
-                $replace      = (string) $level . " DATE " . $date_value . "\n" .  (string) ($level + 1) . " PHRASE " . $phrase_value;
-                $gedcom       = str_replace($search, $replace, $gedcom);
-            }			
-        }        
-
-		//DATE INT (date interpretation)
-        $preg_pattern = [
-			"/([\d]) DATE INT ([^\(]+) \(([^)]+)\)\n/",
-        ];
-
-        foreach ($preg_pattern as $pattern) {
-
-            preg_match_all($pattern, $gedcom, $matches, PREG_SET_ORDER);
-
-            foreach ($matches as $match) {
-                $level = (int) $match[1];
-
-                $date_value   = $match[2];
-                $phrase_value = 'interpratation: ' . $match[3];
-                $search       = (string) $level . " DATE INT " . $date_value . ' (' . $match[3] . ')';
-                $replace      = (string) $level . " DATE " . $date_value . "\n" .  (string) ($level + 1) . " PHRASE " . $phrase_value;
-                $gedcom       = str_replace($search, $replace, $gedcom);
-            }			
-        }        
-           
-		//Languages
-		//Allowed GEDCOM 7 language tags: https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
-
-		preg_match_all("/([12]) LANG (.[^\n]+)\n/", $gedcom, $matches, PREG_SET_ORDER);
-
+		$iana_language_registry_file_name = __DIR__ . '/../../vendor/iana/iana_languages.txt';
+		$iana_language_registry = file_get_contents($iana_language_registry_file_name);
+	 
+		//Create language table
+		preg_match_all("/Type: language\nSubtag: ([^\n]+)\nDescription: ([^\n]+)\n/", $iana_language_registry, $matches, PREG_SET_ORDER);
+	 
 		foreach ($matches as $match) {
+		   $this->language_to_code_table[strtoupper($match[2])]= $match[1];
+		}   
+	}
 
-			if (isset($this->language_to_code_table[strtoupper($match[2])])) {
+    /**
+     * Custom conversion of a Gedcom string
+     *
+     * @param string $pattern       The pattern of the filter rule, e. g. INDI:*:DATE
+     * @param string $gedcom        The Gedcom to convert
+     * @param array  $records_list  A list with all xrefs and the related records: array <string xref => Record record>
+     * 
+     * @return string               The converted Gedcom
+     */
+    public function customConvert(string $pattern, string $gedcom, array $records_list): string {
 
-				$search =  $match[1] . " LANG " . $match[2] . "\n";
-				$replace = $match[1] . " LANG " . $this->language_to_code_table[strtoupper($match[2])] . "\n";
-				$gedcom = str_replace($search, $replace, $gedcom);
+		if (strpos($pattern, ':DATE') > 0) {
+
+			//Specific dates with slashes in years, eg. 1741/42
+			$preg_pattern = [
+				"/([\d]) DATE ([\d]{0,2})([ ]*)(|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)([ ]*)([\d]{1,4})\/([\d]{2})\n/",
+			];
+
+			foreach ($preg_pattern as $pattern) {
+
+				preg_match_all($pattern, $gedcom, $matches, PREG_SET_ORDER);
+
+				foreach ($matches as $match) {
+					$level = (int) $match[1];
+
+					$date_value   = $match[2] . $match[3] . $match[4] . $match[5]  . $match[6];
+					$phrase_value = $date_value . "/" . $match[7];
+					$search       = (string) $level . " DATE " . $phrase_value;
+					$replace      = (string) $level . " DATE " . $date_value . "\n" .  (string) ($level + 1) . " PHRASE " . $phrase_value;
+					$gedcom       = str_replace($search, $replace, $gedcom);
+				}			
+			}        
+
+			//DATE INT (date interpretation)
+			$preg_pattern = [
+				"/([\d]) DATE INT ([^\(]+) \(([^)]+)\)\n/",
+			];
+
+			foreach ($preg_pattern as $pattern) {
+
+				preg_match_all($pattern, $gedcom, $matches, PREG_SET_ORDER);
+
+				foreach ($matches as $match) {
+					$level = (int) $match[1];
+
+					$date_value   = $match[2];
+					$phrase_value = 'interpratation: ' . $match[3];
+					$search       = (string) $level . " DATE INT " . $date_value . ' (' . $match[3] . ')';
+					$replace      = (string) $level . " DATE " . $date_value . "\n" .  (string) ($level + 1) . " PHRASE " . $phrase_value;
+					$gedcom       = str_replace($search, $replace, $gedcom);
+				}			
+			}        
+		}
+		elseif (strpos($pattern, ':LANG') > 0) {
+
+			//Languages
+			//Allowed GEDCOM 7 language tags: https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+
+			preg_match_all("/([12]) LANG (.[^\n]+)\n/", $gedcom, $matches, PREG_SET_ORDER);
+
+			foreach ($matches as $match) {
+
+				if (isset($this->language_to_code_table[strtoupper($match[2])])) {
+
+					$search =  $match[1] . " LANG " . $match[2] . "\n";
+					$replace = $match[1] . " LANG " . $this->language_to_code_table[strtoupper($match[2])] . "\n";
+					$gedcom = str_replace($search, $replace, $gedcom);
+				}
 			}
 		}
+		elseif (in_array($pattern, ['INDI:NAME:TYPE', 'INDI:FAMC:STAT'])) {
 
-		//Enumsets
+			//Nested enumsets
 
-		$enumsets = [
-			"ADOP" => ["HUSB", "WIFE", "BOTH",],
-			"MEDI" => ["AUDIO", "BOOK","CARD", "ELECTRONIC", "FICHE", "FILM", "MAGAZINE", "MANUSCRIPT", "MAP", "NEWSPAPER", "PHOTO", "TOMBSTONE", "VIDEO", "OTHER",],
-			"PEDI" => ["ADOPTED", "BIRTH", "FOSTER", "SEALING", "OTHER",],
-			"QUAY" => ["1", "2", "3",],
-			"RESN" => ["CONFIDENTIAL", "LOCKED", "PRIVACY",],
-			"ROLE" => ["CHIL", "CLERGY", "FATH", "FRIEND", "GODP", "HUSB", "MOTH", "MULTIPLE", "NGHBR", "OFFICIATOR", "PARENT", "SPOU", "WIFE", "WITN", "OTHER",],
-			"SEX" =>  ["M", "F", "X", "U",],
-		];
+			foreach (self::NESTED_ENUMSETS as $enumset) {
 
-		foreach ($enumsets as $enumset => $values) {
+				$tags = $enumset["tags"];
+				$enum_values = $enumset["values"];
+				$level1_tag = $tags[0];
+				$level2_tag = $tags[1];
 
-			preg_match_all("/([\d]) " . $enumset . " (.[^\n]+)/", $gedcom, $matches, PREG_SET_ORDER);
+				if ($pattern === 'INDI:' . $level1_tag . ':'. $level2_tag) {
 
-			foreach ($matches as $match) {
-				$level = (int) $match[1];
+					preg_match_all("/2 " . $level2_tag . " (.*)/", $gedcom, $matches, PREG_SET_ORDER);
 
-				//If allowed value, convert to upper case
-				if (in_array(strtoupper($match[2]), $values)) {
-					$search =  (string) $level . " " . $enumset . " " . $match[2];
-					$replace = (string) $level . " " . $enumset . " " . strtoupper($match[2]);
-					$gedcom = str_replace($search, $replace, $gedcom);
-				}
-				//If phrase is allowed for this enumtype, use phrase instead 
-				elseif (in_array($enumset, ["ADOP", "MEDI", "PEDI", "ROLE"])){
-					$search =  (string) $level . " " . $enumset . " " . $match[2];
-                    //For specific role descriptions
-                    if ($enumset == "ROLE") {
-                        $match[2] = str_replace(['(', ')'], ['', ''], $match[2]);  // (<ROLE_DESCRIPTOR>)
-                    }
-					$replace = (string) $level . " " . $enumset . " OTHER\n" . (string) ($level + 1) . " PHRASE " . $match[2];
-					$gedcom = str_replace($search, $replace, $gedcom);
+					foreach ($matches as $match) {
+	
+						$found_type =  $match[1];		
+	
+						//If allowed type
+						if (in_array(strtoupper($found_type), $enum_values)) {
+							$search =  "2 " . $level2_tag . " " . $found_type;
+							$replace = "2 " . $level2_tag . " " . strtoupper($found_type);
+							$gedcom = str_replace($search, $replace, $gedcom);
+						}
+						//Use OTHER/PHRASE instead
+						else {
+							$search =  "2 " . $level2_tag . " " . $found_type;
+							$replace = "2 " . $level2_tag . " OTHER\n3 PHRASE " . $found_type;
+							$gedcom = str_replace($search, $replace, $gedcom);
+						}
+					}		
 				}
 			}
-		}		
+		}
+		else {
 
-		//Nested enumsets
+			//Enumsets
 
-		$nested_enumsets = [
-			[ "tags" => ["NAME", "TYPE"], "values" => ["AKA", "BIRTH", "IMMIGRANT", "MAIDEN", "MARRIED", "PROFESSIONAL",]],
-			[ "tags" => ["FAMC", "STAT"], "values" => ["CHALLENGED", "DISPROVEN", "PROVEN",]],
-		];
+			foreach (self::ENUMSETS as $enumset => $values) {
 
-		foreach ($nested_enumsets as $enumset) {
+				preg_match_all("/([\d]) " . $enumset . " (.+)/", $gedcom, $matches, PREG_SET_ORDER);
 
-			$tags = $enumset["tags"];
-			$enum_values = $enumset["values"];
-			$level1_tag = $tags[0];
-			$level2_tag = $tags[1];
+				foreach ($matches as $match) {
+					$level = (int) $match[1];
 
-			preg_match_all("/([\d]) " . $level1_tag . " (.[^\n]+)\n([\d]) " . $level2_tag . " (.[^\n]+)/", $gedcom, $matches, PREG_SET_ORDER);
-
-			foreach ($matches as $match) {
-
-				$size = sizeof($match);
-				$level = (int) $match[$size - 2];
-				$found_type =  $match[$size - 1];		
-
-				//If allowed type
-				if (in_array(strtoupper($found_type), $enum_values)) {
-					$search =  (string) $level . " " . $level2_tag . " " . $found_type;
-					$replace = (string) $level . " " . $level2_tag . " " . strtoupper($found_type);
-					$gedcom = str_replace($search, $replace, $gedcom);
+					//If no known ENUM value and phrase is allowed for this enumtype, use phrase instead 
+					if (!in_array(strtoupper($match[2]), $values) && in_array($enumset, ["ADOP", "MEDI", "PEDI", "ROLE"])) {
+						$search =  (string) $level . " " . $enumset . " " . $match[2];
+						//For specific role descriptions
+						if ($enumset == "ROLE") {
+							$match[2] = str_replace(['(', ')'], ['', ''], $match[2]);  // (<ROLE_DESCRIPTOR>)
+						}
+						$replace = (string) $level . " " . $enumset . " OTHER\n" . (string) ($level + 1) . " PHRASE " . $match[2];
+						$gedcom = str_replace($search, $replace, $gedcom);
+					}
+					//Anyway, convert to upper case
+					else {
+						$search =  (string) $level . " " . $enumset . " " . $match[2];
+						$replace = (string) $level . " " . $enumset . " " . strtoupper($match[2]);
+						$gedcom = str_replace($search, $replace, $gedcom);
+					}					
 				}
-				//Use OTHER/PHRASE instead
-				else {
-					$search =  (string) $level  . " " . $level2_tag . " " . $found_type;
-					$replace = (string) $level  . " " . $level2_tag . " OTHER\n" . (string) ($level + 1) . " PHRASE " . $found_type;
-					$gedcom = str_replace($search, $replace, $gedcom);
-				}
-			}	
-		}	
+			}		
+		}
 
 		return $gedcom;
 	}

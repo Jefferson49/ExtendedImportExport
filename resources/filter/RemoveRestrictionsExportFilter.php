411f2c3b4647cbc5e06b7c0ba3b75c3b1530b4ae
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-07-04 16:50:16 +0200

    Added export filter to remove RESN

---
+++ resources/filter/RemoveRestrictionsExportFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jefferson49\Webtrees\Module\DownloadGedcomWithURL;
+
+/**
+ * An export filter, which removes RESN structures
+ */
+class RemoveRestrictionsExportFilter extends AbstractExportFilter implements ExportFilterInterface
+{    
+    protected const EXPORT_FILTER_RULES = [
+        //GEDCOM tag to be exported => Regular expression to be applied for the chosen GEDCOM tag
+        //                             ["search pattern" => "replace pattern"],
+
+        //Remove RESN structures
+        '!*:RESN'                   => [],
+        '!*:*:RESN'                 => [],
+        '!*:*:*:RESN'               => [],
+
+        //Export other structures      
+        '*'                         => [],
+    ];
+}

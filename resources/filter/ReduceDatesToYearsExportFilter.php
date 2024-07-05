cf95feda1d5d57cf8a4ba03747cc41da6489be38
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-07-05 05:41:33 +0200

    Feature: Added export filter to reduce dates to years

---
+++ resources/filter/ReduceDatesToYearsExportFilter.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jefferson49\Webtrees\Module\DownloadGedcomWithURL;
+
+use Fisharebest\Webtrees\Tree;
+
+/**
+ * An export filter, which reduces all dates to years only
+ * 
+ */
+class ReduceDatesToYearsExportFilter extends AbstractExportFilter implements ExportFilterInterface
+{
+    protected const EXPORT_FILTER_RULES = [
+      
+        //GEDCOM tag to be exported => Regular expression to be applied for the chosen GEDCOM tag
+        //                             ["search pattern" => "replace pattern"],
+
+        //Do not change dates in HEAD
+        'HEAD:DATE'                 => [],
+        'HEAD:SOUR:DATA:DATE'       => [],
+
+        //Do not change dates in sources and source citations
+        'SOUR:DATA:EVEN:DATE'       => [],
+        'SOUR:DATA:DATE'            => [],
+
+        //Do not change dates in CHAN (change) records
+        '*:CHAN:DATE'               => [],
+
+        //Do not change dates in _LOC records
+        '_LOC:NAME:DATE'            => [],
+
+        //Convert all other dates
+        '*:DATE'                 	=> ["RegExp_macro" => "DateToYear"],
+        '*:*:DATE'                 	=> ["RegExp_macro" => "DateToYear"],
+        '*:*:*:DATE'                => ["RegExp_macro" => "DateToYear"],
+        '*:*:*:*:DATE'              => ["RegExp_macro" => "DateToYear"],
+        
+        //Export other structures      
+        '*'                         => [],        
+    ];
+
+    protected const REGEXP_MACROS = [
+        //Name                      => Regular expression to be applied for the chosen GEDCOM tag
+        //                             ["search pattern" => "replace pattern"],
+
+        "DateToYear"                => ["([\d]) DATE (INT )*(ABT |CAL |EST |AFT |BEF |BET )*(?:.*([\d]{4} AND ))*.*([\d]{4})( .*)*" => "$1 DATE $2$3$4$5$6"],
+    ];   
+}

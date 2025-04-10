5b8ced06a35548f534034d05c72886e704346064
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2025-04-10 18:55:43 +0200

    Increased version to 4.2.6

--- DownloadGedcomWithURL.php
+++ DownloadGedcomWithURL.php
@@ -150,7 +150,7 @@ class DownloadGedcomWithURL extends AbstractModule implements
     private array $standard_params;
 
 	//Custom module version
-	public const CUSTOM_VERSION = '4.2.5';
+	public const CUSTOM_VERSION = '4.2.6';
 
 	//Routes
 	protected const ROUTE_REMOTE_ACTION_OLD = '/DownloadGedcomWithURL';
